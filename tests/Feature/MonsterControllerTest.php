3b937936e0f86c0d136292d67f00456ff9ab4ded
motoshifurugen <amanogawa_m@icloud.com>
2020-06-14 13:16:55 +0900

    add controller test

---
+++ tests/Feature/MonsterControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MonsterControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $response = $this->get('monsters.index');
+
+        $response->assertStatus(200);
+    }
+}
